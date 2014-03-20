80ba180c30fd963705d4ca08a375c1c92141979c
nickjadams <nickjadams@gmail.com>
2014-03-20 15:08:46 +0000

    Improved documentation/comments and added sanity checks where speed is used

--- car/car.ino
+++ car/car.ino
@@ -1,22 +1,37 @@
 /*
- * Car control system.  
+ * Car control system. 
+ * Nick J Adams
+ * 
+ * The motors are connected to two H-Bridge 1A Motor drivers (SN754410), 
+ * the ICs are connected in parallel on the left and on the right. The 
+ * first chip powers the front left motor and the front right motor, the 
+ * second chip powers the rear left motor and the rear right motor. 
+ * In this control system the two left motors and two right motors are 
+ * controled together. This code therefore works equally well for both
+ * two and four wheeled vehicles.
  */
  
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
+// Pin assignments
 int led = 13;
-
 int speedLeftPin = 3;
 int speedRightPin = 6;
-
 int controlOneA = 4;
 int controlTwoA = 5;
 int controlThreeA = 7;
 int controlFourA = 8;
 
+// Global Variables
 int currentSpeed = 0;
 
-// the setup routine runs once when you press reset:
+
+/*
+ * This method is run once when the Arduino is powered or
+ * it's reset putton is pressed. 
+ * 
+ * This method configures mode of the pins, it ensures all the motors 
+ * start in a stopped state and it assignes an initial value to the 
+ * speed variable. 
+ */
 void setup() {                
   // initialize the digital pin as an output.
   pinMode(led, OUTPUT);
@@ -34,7 +49,13 @@ void setup() {
   currentSpeed=255;
 }
 
-// the loop routine runs over and over again forever:
+/*
+ * This is the main guts of the program, it will run this method over 
+ * and over until the power is removed or the reset puttion is pressed. 
+ *
+ * This is where the behaviour of the car is defined. it makes use of the delay 
+ * function to carryout the specified behaviour for the given amount of time. 
+ */
 void loop() {
   moveForward(currentSpeed);
   delay(1000);                   
@@ -56,7 +77,13 @@ void loop() {
  * speed is provided then the motor will stall. 
  */
 void moveForward(int speed){
-  currentSpeed = speed;
+  if (speed > 255){
+    currentSpeed = 255;
+  } else if (speed <= 0){
+    currentSpeed = 0;
+  } else {
+    currentSpeed = speed;
+  }
   digitalWrite(controlOneA, HIGH);  
   digitalWrite(controlTwoA, LOW); 
   digitalWrite(controlThreeA, HIGH); 
@@ -71,7 +98,13 @@ void moveForward(int speed){
  * speed is provided then the motor will stall. 
  */
 void moveBackwards(int speed){
-  currentSpeed = speed;
+  if (speed > 255){
+    currentSpeed = 255;
+  } else if (speed <= 0){
+    currentSpeed = 0;
+  } else {
+    currentSpeed = speed;
+  }
   digitalWrite(controlOneA, LOW);  
   digitalWrite(controlTwoA, HIGH); 
   digitalWrite(controlThreeA, LOW); 
@@ -82,10 +115,16 @@ void moveBackwards(int speed){
 }
 
 /*
- * Spin the car right
+ * Spin the car right at the specified speed
  */
 void spinRight(int speed){
-  currentSpeed = speed;
+  if (speed > 255){
+    currentSpeed = 255;
+  } else if (speed <= 0){
+    currentSpeed = 0;
+  } else {
+    currentSpeed = speed;
+  }
   analogWrite(speedLeftPin, 0);
   analogWrite(speedRightPin, 0);
   digitalWrite(controlOneA, HIGH);  
@@ -98,10 +137,16 @@ void spinRight(int speed){
 }
 
 /*
- * spin the car left
+ * spin the car left at the specified speed
  */
 void spinLeft(int speed){
-  currentSpeed = speed;
+  if (speed > 255){
+    currentSpeed = 255;
+  } else if (speed <= 0){
+    currentSpeed = 0;
+  } else {
+    currentSpeed = speed;
+  }
   analogWrite(speedLeftPin, 0);
   analogWrite(speedRightPin, 0);
   digitalWrite(controlOneA, LOW);  
@@ -123,6 +168,9 @@ void allStop(){
   digitalWrite(led, LOW);
 }
 
+/*
+ * Increase the speed by 10 (about 4%), will increase speed to maximum 255 then stop. 
+ */
 void speedUp(){
   if (currentSpeed < 245){
     currentSpeed += 10;
@@ -134,6 +182,9 @@ void speedUp(){
   analogWrite(speedRightPin, currentSpeed);
 }
 
+/* 
+ * Decrease the speed by 10 (about 4%), will decrease the speed to 0 then stop. 
+ */
 void slowDown(){
   if (currentSpeed > 10) {
     currentSpeed -= 10;
